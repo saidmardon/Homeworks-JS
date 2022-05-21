c444aeebdba359e6309cbab65e36a850e3967618
Saymumin <mardonov240687@gmail.com>
2022-05-21 15:11:28 +0500

    added new tasks

--- Lesson_5_Data types.js/String.js/Task_5_slice.js
+++ Lesson_5_Data types.js/String.js/Task_5_slice.js
@@ -4,4 +4,5 @@ console.log(text.slice(1,3)) //ay
 console.log(text.slice(-3,-2)) //m
 console.log(text.slice(-3,-1)) //mi
 console.log(text.slice(-1)) //n
+
 //возврашает часть строки от start до end, причем end не включается
--- Lesson_5_Data types.js/String.js/Task_6_toLowerCase.js
+++ Lesson_5_Data types.js/String.js/Task_6_toLowerCase.js
@@ -1,3 +1,2 @@
 let text = "Hello"
-console.log(text.toLocaleUpperCase()) //HELLO
 console.log(text.toLowerCase()) //hello
